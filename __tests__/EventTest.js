7122084806d67bc52dea28d6a863fead9b0c6f06
jaeml00 <107801932+jaeml06@users.noreply.github.com>
2023-11-11 19:20:15 +0900

    test: isCheckGivewayEvent

--- __tests__/EventTest.js
+++ __tests__/EventTest.js
@@ -5,4 +5,9 @@ describe('Event class 검사', () => {
     const temp = new Event(3, [['바비큐립', 3], ['초코케이크', 2]]);
     expect(temp.calculateTotalPrice()).toEqual(192000);
   });
+
+  test('총 주문 금액 12만원 시 증정 메뉴 증정', () => {
+    const temp = new Event(3, [['바비큐립', 3], ['초코케이크', 2]]);
+    expect(temp.isCheckGiveawayEvent()).toEqual(true);
+  });
 });
